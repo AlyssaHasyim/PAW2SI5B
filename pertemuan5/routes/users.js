7853e942647b402c6a85e4db9f5990887266aa8e
Alyssa Hasyim <alyssahasyim@gmail.com>
2024-09-24 15:00:48 -0800

    pertemuan5

---
+++ pertemuan5/routes/users.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.use('/user',(eq, res, next)=>{
+  console.log('Router middleware - request URL' + req.originalUrl)
+  next()
+},(req, res, next)=>{
+  console.log('Router Middleware - request type' + req.method)
+  next()
+});
+
+router.get('/user/:id', (req, res, next) => {
+  // if the user ID is 0, skip to the next router
+  if (req.params.id === '0') next('route')
+  // otherwise pass control to the next middleware function in this stack
+  else next()
+}, (req, res, next) => {
+  // render a regular page
+  res.render('regular')
+})
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', (req, res, next) => {
+  console.log(req.params.id)
+  res.send('special')
+})
+
+module.exports = router;
