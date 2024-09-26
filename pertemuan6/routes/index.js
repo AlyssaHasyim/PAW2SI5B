4e6a6573a1092c180c4a6255aa452ceb81c2da69
Alyssa Hasyim <alyssahasyim@gmail.com>
2024-09-26 13:06:09 -0800

    pertemuan6

---
+++ pertemuan6/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/post', (req, res) => {
+  res.render('post');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+module.exports = router;
